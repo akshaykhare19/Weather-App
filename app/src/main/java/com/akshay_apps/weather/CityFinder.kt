0c7e9c3dcb16c26768b131ef55c18b7df5b6ebc4
Akshay Khare <akhare931@gmail.com>
2021-05-09 22:37:45 +0530

    Initial Commit

---
+++ app/src/main/java/com/akshay_apps/weather/CityFinder.kt
@@ -0,0 +1,11 @@
+package com.akshay_apps.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CityFinder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_finder)
+    }
+}
